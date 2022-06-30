363ceed42f0d1f9c8722404ee6b743cd5a5ee252
Mohammed Alshahrani <mmogib@gmail.com>
2022-06-30 16:19:33 +0300

    added airtable

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,6 +1,11 @@
 import StatusCodes from "http-status-codes";
 import { Request, Response, Router } from "express";
+import Airtable from "airtable";
 
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY || "" }).base(
+  process.env.AIRTABLE_BASE || ""
+);
+const table = base("Releases");
 // Constants
 const router = Router();
 const { OK, NO_CONTENT } = StatusCodes;
@@ -75,15 +80,36 @@ const updates: Update = {
 /**
  * Get all users.
  */
-router.get(p.get_verstion, (req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get(p.get_verstion, async (req: Request, res: Response) => {
   const { params } = req;
-  const { version } = params;
-  const new_version: UpdaterResponse = updates[version];
-  if (!new_version) {
+  const { version, target } = params;
+  const records = await table
+    .select({
+      filterByFormula: `AND({key}="${version}",{target}="${target}")`,
+    })
+    .all();
+  if (!records || records.length == 0) {
     return res.status(NO_CONTENT).json();
   }
+  const record = records[0];
+  const new_version: UpdaterResponse = {
+    notes: record.get("notes") as string,
+    pub_date: new Date(record.get("date") as string).toISOString(),
+    version: record.get("version") as string,
+    platforms: {
+      [target]: {
+        signature: "",
+        url: record.get("url") as string,
+      },
+    },
+  };
+  // const new_version: UpdaterResponse = updates[version];
+  // if (!new_version) {
+  //
+  // }
 
-  return res.status(OK).json(new_version);
+  return res.status(OK).json({ ...new_version });
 });
 
 // Export default

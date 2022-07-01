0e6bba6c3c70d8c214c7db6e2ec6c1ad014b9d48
Mohammed Alshahrani <mmogib@gmail.com>
2022-07-01 07:55:32 +0300

    again with response

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -35,34 +35,40 @@ interface UpdaterResponse {
 router.get(p.get_verstion, async (req: Request, res: Response) => {
   const { params } = req;
   const { version, target } = params;
-  const records = await table
-    .select({
-      filterByFormula: `AND({key}="${version}",{target}="${target}")`,
-    })
-    .all();
-  if (!records || records.length == 0) {
-    return res.status(NO_CONTENT).json({});
-  }
-  const record = records[0];
-  const new_version: UpdaterResponse = {
-    url: record.get("url") as string,
-    version: record.get("version") as string,
-    notes: record.get("notes") as string,
-    pub_date: new Date(record.get("date") as string).toISOString(),
-    signature: record.get("signature") as string,
-  };
-  // const new_version: UpdaterResponse = updates[version];
-  // if (!new_version) {
-  //{
-  //   "url": "https://mycompany.example.com/myapp/releases/myrelease.tar.gz",
-  //   "version": "0.0.1",
-  //   "notes": "Theses are some release notes",
-  //   "pub_date": "2020-09-18T12:29:53+01:00",
-  //   "signature": ""
-  // }
-  // }
+  try {
+    const records = await table
+      .select({
+        filterByFormula: `AND({key}="${version}",{target}="${target}")`,
+      })
+      .all();
+    if (!records || records.length == 0) {
+      console.log("here");
+      return res.status(NO_CONTENT).send("SUCCsESS");
+    }
+    const record = records[0];
+    const new_version: UpdaterResponse = {
+      url: record.get("url") as string,
+      version: record.get("version") as string,
+      notes: record.get("notes") as string,
+      pub_date: new Date(record.get("date") as string).toISOString(),
+      signature: record.get("signature") as string,
+    };
+    // const new_version: UpdaterResponse = updates[version];
+    // if (!new_version) {
+    //{
+    //   "url": "https://mycompany.example.com/myapp/releases/myrelease.tar.gz",
+    //   "version": "0.0.1",
+    //   "notes": "Theses are some release notes",
+    //   "pub_date": "2020-09-18T12:29:53+01:00",
+    //   "signature": ""
+    // }
+    // }
 
-  return res.status(OK).json({ ...new_version });
+    return res.status(OK).json({ ...new_version });
+  } catch (error) {
+    console.log(error);
+    return res.status(NO_CONTENT).send("SUCCsESS");
+  }
 });
 
 // Export default

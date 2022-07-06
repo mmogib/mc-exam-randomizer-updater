5a7e2dc98efff7b81a628d270b40b4318dc08172
Mohammed Alshahrani <mmogib@gmail.com>
2022-07-06 07:45:21 +0300

    added version api endpoint

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,28 +1,23 @@
 import StatusCodes from "http-status-codes";
 import { Request, Response, Router } from "express";
 import Airtable from "airtable";
+import { json } from "stream/consumers";
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY || "" }).base(
   process.env.AIRTABLE_BASE || ""
 );
-const table = base("Releases");
+const releases_table = base("Releases");
+const versions_table = base("Versions");
 // Constants
 const router = Router();
 const { OK, NO_CONTENT } = StatusCodes;
 
 // Paths
 export const p = {
-  get_verstion: "/:target/:version",
+  get_release: "/:target/:version",
+  get_version: "/version",
 } as const;
-/*
- * {
-  "url": "https://mycompany.example.com/myapp/releases/myrelease.tar.gz",
-  "version": "0.0.1",
-  "notes": "Theses are some release notes",
-  "pub_date": "2020-09-18T12:29:53+01:00",
-  "signature": ""
-}
- */
+
 interface UpdaterResponse {
   url: string;
   version: string;
@@ -32,17 +27,42 @@ interface UpdaterResponse {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.get(p.get_verstion, async (req: Request, res: Response) => {
+router.get(p.get_version, async (_req: Request, res: Response) => {
+  try {
+    const records = await versions_table
+      .select({
+        filterByFormula: `{status}="stable"`,
+      })
+      .all();
+    if (!records || records.length == 0) {
+      throw new Error("No stable versions found");
+    }
+    const recs = records.map((r) => r.get("version"));
+    if (recs && recs.length > 0) {
+      const version = recs.sort((a, b) => (a && b && a > b ? -1 : 1))[0];
+      return res.status(OK).json({ status: "success", data: version });
+    } else {
+      throw new Error("No stable versions found");
+    }
+  } catch (error) {
+    return res.status(OK).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get(p.get_release, async (req: Request, res: Response) => {
   const { params } = req;
   const { version, target } = params;
   try {
-    const records = await table
+    const records = await releases_table
       .select({
         filterByFormula: `AND({key}="${version}",{target}="${target}")`,
       })
       .all();
     if (!records || records.length == 0) {
-      console.log("here");
       return res.status(NO_CONTENT).send("SUCCsESS");
     }
     const record = records[0];
@@ -53,20 +73,9 @@ router.get(p.get_verstion, async (req: Request, res: Response) => {
       pub_date: new Date(record.get("date") as string).toISOString(),
       signature: record.get("signature") as string,
     };
-    // const new_version: UpdaterResponse = updates[version];
-    // if (!new_version) {
-    //{
-    //   "url": "https://mycompany.example.com/myapp/releases/myrelease.tar.gz",
-    //   "version": "0.0.1",
-    //   "notes": "Theses are some release notes",
-    //   "pub_date": "2020-09-18T12:29:53+01:00",
-    //   "signature": ""
-    // }
-    // }
 
     return res.status(OK).json({ ...new_version });
   } catch (error) {
-    console.log(error);
     return res.status(NO_CONTENT).send("SUCCsESS");
   }
 });
